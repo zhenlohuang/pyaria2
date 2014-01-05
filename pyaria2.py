a809721d879dcd34816e82ad77c4fa5491c5f86e
killuahzl <killua_hzl@163.com>
2014-01-05 17:06:50 +0800

    Modify module description.

--- pyaria2.py
+++ pyaria2.py
@@ -20,7 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
-Description: PyAria2 is a Python 3 module that provides a wrapper class around Aria2's RPC interface. It can be used to build applications that use Aria2 for downloading data.
+Description: pyaria2 is a Python 3 module that provides a wrapper class around Aria2's RPC interface. It can be used to build applications that use Aria2 for downloading data.
 Author: Killua
 Email: killua_hzl@163.com
 '''
